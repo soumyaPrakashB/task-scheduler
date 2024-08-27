e54b65654a065c33d4d3c74f2e83cf2c54bf3477
Soumya Behera <lpsoumyaprakash@gmail.com>
2024-08-27 12:13:43 +0530

    first commit

---
+++ src/test/java/com/soumya/taskscheduler/TaskschedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soumya.taskscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskschedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
